45c6b685552aea19738acf1aba33bb471f98d305
jihye kang <jihye59kang@gmail.com>
2022-05-09 17:32:09 +0900

    algorithm section01-03,04

---
+++ Algorithm/JS/section1/04.js
@@ -0,0 +1,18 @@
+// 자연수 N이 입력되면 1부터 N까지의 합을 출력하는 프로그램을 작성하세요.
+
+function solution(n){
+    // 내가 쓴 풀이
+    let answer=0;
+    for(let i =0; i < n; i++){
+        answer += (n - i);
+    }
+
+    /* 
+    for(let i=1; i<=n; i++){
+        answer=answer+i;
+    }
+    */
+    return answer;
+}
+
+console.log(solution(6));
