176a6028cdbc0576105b5d35394c06a5c12fe141
jihye kang <jihye59kang@gmail.com>
2022-05-20 23:59:10 +0900

    Algorithm 자료구조-2

---
+++ Algorithm/JS/자료구조/03.js
@@ -0,0 +1,31 @@
+// 크레인 인형뽑기(카카오 기출)
+
+function solution(board, moves){
+    let answer = 0;
+    let stack = [];
+
+    moves.forEach(m => {
+        for(let i=0; i<board.length; i++){
+            if(board[i][m -1]!== 0){
+                let temp = board[i][m -1];
+                board[i][m -1] = 0;
+                if(temp === stack[stack.length -1]){
+                    stack.pop()
+                    answer += 2;
+                }
+                else stack.push(temp)
+                break;
+            }
+        }
+    })
+
+    return answer;
+}
+
+let a=[[0,0,0,0,0],
+        [0,0,1,0,3],
+        [0,2,5,0,1],
+        [4,2,4,4,2],
+        [3,5,1,3,1]];
+let b=[1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
