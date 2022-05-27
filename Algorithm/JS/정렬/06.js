d7e4f20feaa25c7d8806243cc07a506174a63088
jihye kang <jihye59kang@gmail.com>
2022-05-27 23:34:09 +0900

    Algorithm 정렬

---
+++ Algorithm/JS/정렬/06.js
@@ -0,0 +1,16 @@
+// 장난꾸러기 현수
+
+function solution(arr){
+    let answer = [];
+    let newArr = arr.slice();
+    newArr.sort((a, b) => a-b)
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== newArr[i]) answer.push(i+1)
+    }
+
+    return answer;
+}
+
+let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr));
