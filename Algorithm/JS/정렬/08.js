1b9136c353ba118347b69c0bb4776f1729cc0838
jihye kang <jihye59kang@gmail.com>
2022-05-29 22:14:26 +0900

    Algorithm 정렬

--- Algorithm/JS/정렬/08.js
+++ Algorithm/JS/정렬/08.js
@@ -1,1 +1,22 @@
 // 회의실 배정
+// 각 회의에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하 면서 회의실을 사용할 수 있는 최대수의 회의를 찾아라. 
+
+function solution(meeting){
+    let answer=0;
+    meeting.sort((a, b) => {
+        if(a[1] === b[1]) return a[0] - b[0]
+        else return a[1] - b[1]
+    })
+    console.log(meeting)
+    let end = 0;
+    for(let x of meeting){
+        if(x[0] >= end){
+            answer++;
+            end = x[1];
+        }
+    }
+    return answer;
+}
+
+let arr=[[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]];
+console.log(solution(arr));
